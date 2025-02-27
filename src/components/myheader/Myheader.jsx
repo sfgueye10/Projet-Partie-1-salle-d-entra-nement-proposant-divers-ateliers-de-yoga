cabcb8390619f34e9d3be25d48808a9971739008
sfgueye10 <2715320@collegelacite.ca>
2025-02-26 21:41:29 -0500

    Update Myheader.jsx

--- src/components/myheader/Myheader.jsx
+++ src/components/myheader/Myheader.jsx
@@ -1,17 +1,1 @@
-import React from "react";
-import Logo from "./Logo";
-import Menu from "./menu/Menu";
-import styles from "./Myheader.module.css";
 
-const Myheader = (props) => {
-	console.log(props);
-
-	return (
-		<div className={styles.Myheader}>
-			<Logo />
-			<Menu />
-		</div>
-	);
-};
-
-export default Myheader;

970d7a4e89041b0ad0345aa2d1b816aa0651d61b
sfgueye10 <2715320@collegelacite.ca>
2025-02-26 21:41:13 -0500

    Update Logo.jsx

--- src/components/myheader/Logo.jsx
+++ src/components/myheader/Logo.jsx
@@ -1,15 +1,1 @@
-import React from "react";
 
-const Logo = () => {
-  return (
-    <div>
-      <img
-        src="/images/logo.png"
-        alt="Logo"
-        style={{ width: "100px", height: "auto" }}
-      />
-    </div>
-  );
-};
-
-export default Logo;
